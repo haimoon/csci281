0d7b9e014fe5d9e1b8877e70b67878a354d71ddc
haimoon <nikenatural@yahoo.com>
2015-03-23 15:57:45 -0500

    Finish lab 10 Ctype functions

--- Lab_Works/lab_10_ctype_functions/lab_10_ctype_functions/lab_10_ctype_functions.cpp
+++ Lab_Works/lab_10_ctype_functions/lab_10_ctype_functions/lab_10_ctype_functions.cpp
@@ -29,7 +29,7 @@ int main(void)
 		printf("\n %c _ %c", ch, ch);
 	}
 
-// Experiment 03: Print out the ASCII codes of every digit
+//	Experiment 03: Print out the ASCII codes of every digit
 	printf("\n Experiment 03 \n");
 	for (i = 0; i <= 127; i++)
 	{
@@ -39,7 +39,7 @@ int main(void)
 		}
 	}
 
-// Experiment 04: Print out the ASCII codes of every letter/digit
+//	Experiment 04: Print out the ASCII codes of every letter/digit
 	printf("\n Experiment 04: Print out the ASCII codes of every letter/digit \n");
 	for (i = 0; i <= 127; i++)
 	{
@@ -49,12 +49,89 @@ int main(void)
 		}
 	}
 
-// Experiment 05: Input a letter, and print its uppercase version
+//	Experiment 05: Input a letter, and print its uppercase version
+
+	//	Method 01: This does NOT work as it supposes to work. So far using Method 02
+
+	
 	printf("\n Experiment 05: Input a letter, and print its uppercase version \n");
+/*
 	char my_char_variable;
 	printf("\n Please enter a character variable, and I will convert it to uppercase, if necessary: ");
 	scanf_s("%c", &my_char_variable);
-//	printf("\n The variable, in uppercase form, is %c", toupper(my_char_variable)); // Need to fix //
+	printf("\n The variable, in uppercase form, is %c", toupper(my_char_variable)); // Need to fix //
+*/
+
+	//	Method 02:
+	for (i = 0; i <= 127; i++)
+	{
+		if(islower(i))
+		{
+			printf("\n LOWER %d %c\t UPPER %d %c", i, i, toupper(i), toupper(i));
+		}
+	}
+
+
+//	Experiment 06: Print all the lowercase letters
+	printf("\n Experiment 06: Print all the lowercase letters \n");
+	for (i = 0; i <= 127; i++)
+	{
+		if(islower(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
+//	Experiment 07: Print all the uppercase letters
+	printf("\n Experiment 07: Print all the uppercase letters \n");
+	for (i = 0; i <= 127; i++)
+	{
+		if(isupper(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
+//	Experiment 08: Print all alphanumeric characters
+	printf("\n Experiment 08: Print all alphanumeric characters \n");
+	for (i = 0; i <= 127;  i++)
+	{
+		if(isalnum(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
+//	Experiment 09: Print all whitespace characters
+	printf("\n Experiment 09: Print all whitespace characters \n");
+	for (i = 0; i <= 127; i++)
+	{
+		if(isspace(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
+//	Experiment 10: Print all punctuation characters
+	printf("\n Experiment 10: Print all punctuation characters \n");
+	for (i = 0; i <= 127; i++)
+	{
+		if(ispunct(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
+//	Experiment 11: Print all printale characters
+	printf("\n Experiment 11: Print all printable characters \n");
+	for (i = 0; i <= 127; i++)
+	{
+		if(isprint(i))
+		{
+			printf("\n %d %c", i, i);
+		}
+	}
+
 
 	getchar();getchar();
 	return 0;
